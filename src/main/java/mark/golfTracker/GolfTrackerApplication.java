aa2587b6db32fb26ddc0809a65f266037e23d4d2
meloegel <meloegel@gmail.com>
2022-06-29 21:58:58 -0400

    Initial Commit

---
+++ src/main/java/mark/golfTracker/GolfTrackerApplication.java
@@ -0,0 +1,13 @@
+package mark.golfTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GolfTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GolfTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mark/golfTracker/GolfTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package mark.golfTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GolfTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
