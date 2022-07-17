6a14d64bc2ec3ee666b378c2bf516a9029ef8c79
meloegel <meloegel@gmail.com>
2022-07-16 21:23:10 -0400

    Added addNewUser and updateUser in UserController

--- src/main/java/mark/golfTracker/controllers/UserController.java
+++ src/main/java/mark/golfTracker/controllers/UserController.java
@@ -3,14 +3,22 @@ package mark.golfTracker.controllers;
 import mark.golfTracker.models.User;
 import mark.golfTracker.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 
@@ -52,4 +60,36 @@ public class UserController {
         List<User> user = userService.findByUsernameContaining(userName);
         return new ResponseEntity<>(user, HttpStatus.OK);
     }
+
+    // Given a complete User Object, create a new User record and user role records.
+    // http://localhost:2019/users/user
+    // @param newuser A complete new user to add including roles. (role must already exist.)
+    // @return A location header with the URI to the newly created user and a status of CREATED
+    @PostMapping(value = "/user", consumes = "application/json")
+    public ResponseEntity<?> addNewUser(@Valid @RequestBody User newUser) throws URISyntaxException {
+        newUser.setUserId(0);
+        newUser = userService.save(newUser);
+        HttpHeaders responseHeaders = new HttpHeaders();
+        URI newUserURI = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{userId}")
+                .buildAndExpand(newUser.getUserId())
+                .toUri();
+        responseHeaders.setLocation(newUserURI);
+        return new ResponseEntity<>(newUser, responseHeaders, HttpStatus.CREATED);
+    }
+
+    // Given a complete User Object, Given the user id, primary key, is in the User table,
+    //  ** Roles are handled through different endpoints **
+    // Link: http://localhost:2019/users/user/15
+    // @param updateUser - A complete User including
+    //           roles to be used to replace the User. Roles must already exist.
+    // @param userid     The primary key of the user you wish to replace.
+    @PutMapping(value = "/user/{userId}", consumes = "application/json")
+    public ResponseEntity<?> updateUser(@Valid @RequestBody User updatedUser, @PathVariable long userId) {
+        updatedUser.setUserId(userId);
+        userService.save(updatedUser);
+        return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+    }
+
+
 }
