11bb2304b711695e95206827875836ee62da53f8
meloegel <meloegel@gmail.com>
2022-07-16 21:43:56 -0400

    Added getRoleByName, addNewRole and updateRole to RoleController

--- src/main/java/mark/golfTracker/controllers/RoleController.java
+++ src/main/java/mark/golfTracker/controllers/RoleController.java
@@ -3,13 +3,20 @@ package mark.golfTracker.controllers;
 import mark.golfTracker.models.Role;
 import mark.golfTracker.services.RoleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 
 
@@ -34,5 +41,39 @@ public class RoleController {
     public ResponseEntity<?> getRoleById(@PathVariable Long roleId) {
         Role role = roleService.findRoleById(roleId);
         return new ResponseEntity<>(role, HttpStatus.OK);
+    }
+
+
+    // The Role with the given name
+    // Link: http://localhost:2019/roles/role/name/data
+    // @param roleName - The name of the role you seek
+    @GetMapping(value = "/role/name/{roleName}", produces = "application/json")
+    public ResponseEntity<?> getRoleByName(@PathVariable String roleName) {
+        Role role = roleService.findByName(roleName);
+        return new ResponseEntity<>(role, HttpStatus.OK);
+    }
+
+    // Given a complete Role object, create a new Role record
+    // Link: http://localhost:2019/roles/role
+    // @param newRole - A complete new Role object
+    @PostMapping(value = "/role", consumes = "application/json")
+    public ResponseEntity<?> addNewRole(@Valid @RequestBody Role newRole) {
+        newRole.setRoleId(0);
+        newRole = roleService.save(newRole);
+        HttpHeaders responseHeaders = new HttpHeaders();
+        URI newRoleURI = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{roleid}").buildAndExpand(newRole.getRoleId()).toUri();
+        responseHeaders.setLocation(newRoleURI);
+        return new ResponseEntity<>("success", responseHeaders, HttpStatus.CREATED);
+    }
+
+    // The process allows you to update a role name only!
+    // Link: http://localhost:2019/roles/role/3
+    // @param roleid - The primary key (long) of the role you wish to update
+    // @param newRole - The new name (String) for the role
+    @PutMapping(value = "/role/{roleid}", consumes = {"application/json"})
+    public ResponseEntity<?> updateRole(@PathVariable long roleid, @Valid @RequestBody Role newRole) {
+        newRole = roleService.update(roleid, newRole);
+        return new ResponseEntity<>("Success", HttpStatus.OK);
     }
 }
