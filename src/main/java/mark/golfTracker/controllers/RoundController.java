6e4df394b0328f31de51800e1b169285f416dd3f
meloegel <meloegel@gmail.com>
2022-07-21 23:01:35 -0400

    added deleteByRoundId to RoundController

--- src/main/java/mark/golfTracker/controllers/RoundController.java
+++ src/main/java/mark/golfTracker/controllers/RoundController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -82,6 +83,15 @@ public class RoundController {
         newRound.setRoundId(0);
         roundService.save(newRound, userId);
         return new ResponseEntity<>("success", HttpStatus.CREATED);
+    }
+
+    // Deletes a given round
+    // Link: http://localhost:2019/rounds/rounds/4
+    // @param id - The primary key of the round you wish to delete
+    @DeleteMapping(value = "/round/{roundId}")
+    public ResponseEntity<?> deleteByRoundId(@PathVariable long roundId) {
+        roundService.delete(roundId);
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
