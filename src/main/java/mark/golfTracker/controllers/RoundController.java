1d3af98e1c7bee3947c174affbd52804f3b7493b
meloegel <meloegel@gmail.com>
2022-07-21 22:37:04 -0400

    added addNewRound to RoundController

--- src/main/java/mark/golfTracker/controllers/RoundController.java
+++ src/main/java/mark/golfTracker/controllers/RoundController.java
@@ -1,17 +1,24 @@
 package mark.golfTracker.controllers;
 
+import mark.golfTracker.models.Role;
 import mark.golfTracker.models.Round;
 import mark.golfTracker.models.User;
 import mark.golfTracker.services.RoundService;
 import mark.golfTracker.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
 
 
@@ -66,4 +73,15 @@ public class RoundController {
         List<Round> roundList = roundService.findByUser(user);
         return new ResponseEntity<>(roundList, HttpStatus.OK);
     }
+
+    // Given a complete round object, create a new Round record
+    // Link: http://localhost:2019/rounds/rounds/2
+    // @param newRound - A complete new Round object
+    @PostMapping(value = "/rounds/{userId}", consumes = "application/json")
+    public ResponseEntity<?> addNewRound(@Valid @RequestBody Round newRound, @PathVariable Long userId) {
+        newRound.setRoundId(0);
+        roundService.save(newRound, userId);
+        return new ResponseEntity<>("success", HttpStatus.CREATED);
+    }
+
 }
