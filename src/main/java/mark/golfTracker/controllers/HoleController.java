a2c97f017a4457d485d786ba970e7c7ee171b8ca
meloegel <meloegel@gmail.com>
2022-07-24 20:23:04 -0400

    added getHolesByRound to HoleController

--- src/main/java/mark/golfTracker/controllers/HoleController.java
+++ src/main/java/mark/golfTracker/controllers/HoleController.java
@@ -4,6 +4,7 @@ import mark.golfTracker.models.Hole;
 import mark.golfTracker.models.Round;
 import mark.golfTracker.models.User;
 import mark.golfTracker.services.HoleService;
+import mark.golfTracker.services.RoundService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,6 +22,9 @@ public class HoleController {
 
     @Autowired
     private HoleService holeService;
+
+    @Autowired
+    private RoundService roundService;
 
     // Returns a list of all holes
     // Link: http://localhost:2019/holes/holes"
@@ -43,6 +47,15 @@ public class HoleController {
     @GetMapping(value="/holes/holes/par/{par}", produces = "application/json")
     public ResponseEntity<?> getHolesByPar(@PathVariable int par) {
         List<Hole> holeList = holeService.findByPar(par);
+        return new ResponseEntity<>(holeList, HttpStatus.OK);
+    }
+
+    // Return a list of all Holes for a Round
+    // Link: http://localhost:2019/holes/holes/round/3
+    @GetMapping(value="/holes/holes/round/{roundId}", produces = "application/json")
+    public ResponseEntity<?> getHolesByRound(@PathVariable int roundId) {
+        Round round = roundService.findByRoundId(roundId);
+        List<Hole> holeList = holeService.findByRound(round);
         return new ResponseEntity<>(holeList, HttpStatus.OK);
     }
 
